8ccd8b63a71fbe714478b4c20c2d95b2eea825c3
DandyMaulana19 <dandyainul19@gmail.com>
2025-05-24 13:01:48 +0700

    setup zod

---
+++ src/common/pipes/zod.validation.pipe.ts
@@ -0,0 +1,33 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { ZodEffects, ZodObject } from 'zod';
+
+type ZodSchemaType = ZodObject<any> | ZodEffects<ZodObject<any>>;
+
+interface ZodSchemaClass {
+  schema: ZodSchemaType;
+}
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (this.isZodSchema(metadata.metatype)) {
+      const schema = metadata.metatype.schema;
+      const result = schema.safeParse(value);
+
+      if (!result.success) {
+        throw new BadRequestException({
+          message: 'Validation Failed',
+          errors: result.error.errors.map((err) => {
+            path: err.path.join('.');
+            message: err.message;
+          }),
+        });
+      }
+    }
+  }
+}
